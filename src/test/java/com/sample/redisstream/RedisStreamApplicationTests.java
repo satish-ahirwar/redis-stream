c07ac75f81c6f8fc29bbf293fc4e4c98960858ff
satish <satishlariya@gmail.com>
2022-11-02 23:02:50 +0530

    initial commit

---
+++ src/test/java/com/sample/redisstream/RedisStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.redisstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
